7f12eff54897240b651272783c61d0077b80ab80
Kyle <tgen107205@gmail.com>
2025-02-05 23:53:55 +0200

    t3ngho01

---
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.theming.ui.theme.ThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ThemingTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp)
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "My title",
+            modifier = appModifier,
+            style = MaterialTheme.typography.titleLarge
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { /*TODO*/ },
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
+            Text("Submit")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPreview() {
+    ThemingTheme {
+        MyApp()
+    }
+}
